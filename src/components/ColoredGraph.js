883e451484629893fb40ee85663374d80934f98f
Jakob Schödl <jschoedl04@gmail.com>
2022-02-17 21:02:25 +0100

    convert ColoredGraph.js to class component

--- src/components/ColoredGraph.js
+++ src/components/ColoredGraph.js
@@ -1,33 +1,43 @@
 import {ForceGraph2D} from "react-force-graph";
-import React, {useEffect, useState} from "react";
+import React from "react";
 
-export default function ColoredGraph({nodes: initialNodes, links: initialLinks}) {
-    const [nodes, setNodes] = useState(initialNodes)
-    const [links, setLinks] = useState(initialLinks)
+export default class ColoredGraph extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {nodes: this.props.nodes, links: this.props.links}
+    }
 
-    const addNode = () => {
-        setNodes(n => [
-            ...n,
-            {"id": n.length, "group": n.length},
-        ])
-    };
-    useEffect(() => {
-        const highestIndex = nodes.length - 1
-        let newLinks = []
-        const numberOfLinks = Math.floor(Math.random() * nodes.length/3 + 1)
-        for (let i = 0; i < numberOfLinks; i++) {
-            newLinks.push({
-                "source": highestIndex,
-                "target": Math.floor((i + Math.random()) * highestIndex / numberOfLinks)
-            })
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.nodes !== this.state.nodes) {
+            const highestIndex = this.state.nodes.length - 1
+            let newLinks = []
+            const numberOfLinks = Math.floor(Math.random() * this.state.nodes.length / 3 + 1)
+            for (let i = 0; i < numberOfLinks; i++) {
+                newLinks.push({
+                    "source": highestIndex,
+                    "target": Math.floor((i + Math.random()) * highestIndex / numberOfLinks)
+                })
+            }
+            this.setState(previousState => ({links: [...previousState.links, ...newLinks]}))
         }
-        setLinks(l => [...l, ...newLinks])
-    }, [nodes])
-    return <ForceGraph2D
-        graphData={{nodes: nodes, links: links}}
-        nodeAutoColorBy="group"
-        nodeRelSize={7}
-        linkWidth={7}
-        onBackgroundClick={addNode}
-    />
+    }
+
+    render() {
+        const addNode = () => {
+            this.setState(previousState => ({
+                nodes: [
+                    ...previousState.nodes,
+                    {"id": previousState.nodes.length, "group": previousState.nodes.length}
+                ]
+            }))
+        };
+
+        return <ForceGraph2D
+            graphData={{nodes: this.state.nodes, links: this.state.links}}
+            nodeAutoColorBy="group"
+            nodeRelSize={4}
+            linkWidth={6}
+            onBackgroundClick={addNode}
+        />
+    }
 }
