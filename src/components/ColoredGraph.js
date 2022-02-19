26e88602fe2f469396e16ff55b00c04ca909eeb9
Jakob Schödl <jschoedl04@gmail.com>
2022-02-19 13:33:34 +0100

    implement backtracking algorithm to find a proper 4-coloring

--- src/components/ColoredGraph.js
+++ src/components/ColoredGraph.js
@@ -1,6 +1,6 @@
 import {ForceGraph2D} from "react-force-graph";
 import React from "react";
-import {randomInteger} from "../lib/helpers";
+import {randomInteger, withoutDuplicateObjects} from "../lib/helpers";
 
 export const COLORS = [
     "#28536b",
@@ -26,29 +26,66 @@ export default class ColoredGraph extends React.Component {
         this.setDisplayMode = props.setDisplayMode
     }
 
-    findMinimalKColoring() {
-        // TODO: repeat if necessary
-        let anythingChanged = false;
-        let nodesCopy = [...this.state.nodes]
-
-        for (let node of nodesCopy) {
-            let connectingGroups = new Set()
-            for (let edge of this.state.edges) {
-                if (edge.source === node)
-                    connectingGroups.add(edge.target.group)
-                else if (edge.target === node)
-                    connectingGroups.add(edge.source.group)
-            }
-
-            for (let newColor = 0; newColor < node.group; newColor++) {
-                if (!connectingGroups.has(newColor)) {
-                    node.group = newColor
-                    node.color = COLORS[node.group]
-                    this.setState({nodes: nodesCopy})
-                    anythingChanged = true;
-                }
+    static isProperColoring(nodes, edges) {
+        for (let node of nodes) {
+            for (let edge of this.incidentEdges(node, edges)) {
+                if (edge.source.group === edge.target.group)
+                    return false
             }
         }
+        return true;
+    }
+
+    static idIsIncluded(nodes, id) {
+        for (let node of nodes)
+            if (node.id === id || node === id)
+                return true
+        return false
+    }
+
+    static containedEdges(nodes, edges) {
+        return edges.filter(edge => (
+            (ColoredGraph.idIsIncluded(nodes, edge.source) && ColoredGraph.idIsIncluded(nodes, edge.target))
+        ))
+    }
+
+    static incidentEdges(node, edges) {
+        return edges.filter(edge => edge.source.id === node.id || edge.target.id === node.id)
+    }
+
+    async setStateAsync(newState) {
+        return new Promise(resolve =>
+            this.setState(newState, resolve)
+        )
+    }
+
+    findKColoring(k) {
+        let nextIndex = 0;
+        let newNodes = [], newEdges = []
+        let initialNodes = [...this.state.nodes], initialEdges = [...this.state.edges]
+
+        while (true) {
+            if (ColoredGraph.isProperColoring(newNodes, newEdges)) {
+                if (initialNodes.length === newNodes.length)
+                    break;
+
+                newNodes.push(initialNodes[nextIndex])
+                newNodes[newNodes.length - 1].group = 0
+                newEdges = ColoredGraph.containedEdges(newNodes, initialEdges)
+                nextIndex++
+            } else {
+                while (newNodes[newNodes.length - 1].group === k - 1) {
+                    newNodes.pop()
+                    nextIndex--
+                }
+                newNodes[newNodes.length - 1].group++
+            }
+        }
+
+        this.setState({
+            nodes: newNodes,
+            edges: initialEdges,
+        }, this.updateColors)
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -64,15 +101,19 @@ export default class ColoredGraph extends React.Component {
         this.setDisplayMode('custom')
         this.setState(prevState => {
             const newId = prevState.nodes.length
-            const numberOfEdges = randomInteger(1, 2)
-
             let newEdges = []
-            for (let i = 0; i < numberOfEdges; i++) {
-                newEdges.push({
-                    "source": newId,
-                    "target": Math.floor((i + Math.random()) * newId / numberOfEdges)
-                })
+
+            if (prevState.nodes.length) {
+                const numberOfEdges = randomInteger(1, 2)
+                for (let i = 0; i < numberOfEdges; i++) {
+                    newEdges.push({
+                        "source": newId,
+                        "target": randomInteger(0, newId - 1)
+                    })
+                }
             }
+
+            newEdges = withoutDuplicateObjects(newEdges)
 
             return {
                 nodes: [
@@ -86,7 +127,7 @@ export default class ColoredGraph extends React.Component {
                 edges: [...prevState.edges, ...newEdges],
                 numberOfColors: prevState.numberOfColors + 1,
             }
-        }, () => setTimeout(() => this.findMinimalKColoring(), 10))
+        }, () => setTimeout(() => this.findKColoring(4), 10))
     }
 
     render() {
@@ -99,5 +140,16 @@ export default class ColoredGraph extends React.Component {
                 onBackgroundClick={addNode}
             />
         </div>
+    }
+
+    updateColors() {
+        let newNodes = [...this.state.nodes]
+        newNodes.forEach(node => {
+            if (node.group !== undefined)
+                node.color = COLORS[node.group]
+        })
+        this.setState({
+            nodes: newNodes,
+        })
     }
 }
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,3 +1,18 @@
 export function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+export function withoutDuplicateObjects(array) {
+    let result = []
+    for (let index = array.length - 1; index >= 0; index--) {
+        let isUnique = true
+        for (let containedObject of result)
+            if (JSON.stringify(array[index]) === JSON.stringify(containedObject)) {
+                isUnique = false
+                break
+            }
+        if (isUnique)
+            result.push(array[index])
+    }
+    return result
+}
