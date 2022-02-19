b79a1bb0c9b02468478d5ac1b2699e9cc91248cb
Jakob Schödl <jschoedl04@gmail.com>
2022-02-19 09:53:59 +0100

    make the square an actual square

--- src/lib/graphExamples.js
+++ src/lib/graphExamples.js
@@ -13,9 +13,9 @@ export default function getGraph(label) {
                 {id: 3},
             ]
             edges = [
-                {source: 0, target: 1},
-                {source: 1, target: 2},
-                {source: 2, target: 3},
+                {source: 0, target: 2},
+                {source: 2, target: 1},
+                {source: 1, target: 3},
                 {source: 3, target: 0},
             ]
             break
